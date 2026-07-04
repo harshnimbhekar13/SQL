22df3a29ac75a86fe21c91ae3e73a89e6a279137
HARSH NIMBHEKAR <harshnimbhekar890@gmail.com>
2026-07-04 23:12:10 +0530

    Add files via upload

---
+++ SQL Functions (MySQL).sql
@@ -0,0 +1,15 @@
+USE startersql;
+
+-- SELECT * FROM users;
+-- SELECT COUNT(*) FROM users WHERE gender = 'Female';
+-- SELECT MAX(salary) as min_salary,MAX(salary) AS max_salary FROM users;
+-- SELECT SUM(salary) AS total FROM users;
+-- SELECT gender, AVG(salary) AS avg_salary FROM users GROUP BY gender;
+-- SELECT name,LENGTH(name) AS name_len FROM users;
+-- SELECT id,gender,LOWER(name) as lower,CONCAT(LOWER(name),'0108') as username,YEAR(date_of_birth) as YOB, name,LENGTH(name) AS name_len FROM users;
+-- SELECT name,DATEDIFF(CURDATE(),date_of_birth) AS days FROM users;
+-- SELECT salary,
+--  ROUND(salary) AS rounded,
+--  FLOOR(salary) AS floored,
+--  CEIL(salary) AS ceiled
+-- FROM users;
