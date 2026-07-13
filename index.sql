470b68b0384291401146ac707c26258e39c1fda2
HARSH NIMBHEKAR <harshnimbhekar890@gmail.com>
2026-07-13 21:45:18 +0530

    Add files via upload

---
+++ index.sql
@@ -0,0 +1,7 @@
+USE startersql;
+
+SHOW indexes FROM users;
+-- CREATE INDEX idx_email ON users(gender);
+-- CREATE INDEX idx_gender_salary ON users(gender, salary);
+-- SELECT * FROM users WHERE email="aarav@example.com" AND gender = "Male";
+-- DROP INDEX idx_email ON users;
