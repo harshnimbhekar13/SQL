0f9944268979ee1ab70664818946b4a1f03ff578
HARSH NIMBHEKAR <harshnimbhekar890@gmail.com>
2026-07-21 22:52:05 +0530

    Add files via upload

---
+++ PRACTICE01.sql
@@ -0,0 +1,5 @@
+USE startersql;
+-- SELECT * FROM users;
+-- SELECT * FROM users WHERE id<=10;
+-- SELECT * FROM users WHERE date_of_birth IS NOT NULL; 
+SELECT * FROM users WHERE salary BETWEEN '1990-09-09' AND '1999-09-09';
